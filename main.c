98e8e83b50983e5717c6f47fdab3b4e4e08594a7
Bryan-Michael-Tice <bryan.michael.tice@gmail.com>
2020-02-04 19:10:24 -0700

    coded in addition,subtraction,multiplication and power functions into main.c

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 #include "my_complex.h"
 
+int _Add_(int x, int y)
+{
+	return x+y;
+}
+int _Sub_(int x, int y)
+{
+	return x-y;
+}
+int _Multi_(int x, int y)
+{
+	return x * y;
+}
+int power_funct(int x, int y)
+{
+	int i, product=1;
+	for(i=1;i<=y;i++)
+	{
+		product*= x;
+	}
+	return product;
+}
 int main() {
 	Complex a = {3, 4};
 	Complex b = {5, 12};
