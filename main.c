dcb1fc965ff1f8c38f6862c600d7da12355be2f3
Bryan-Michael-Tice <bryan.michael.tice@gmail.com>
2020-02-04 19:52:50 -0700

    modified comments to show function bodies and function calls

--- main.c
+++ main.c
@@ -20,18 +20,23 @@ int power_division(int x, int y)
 }
 
  /*
-   int (*int_fn[2](int,int) = {magnitude, phase};
-    
-       
+ float magnitude(int x, int y)
+ {
+ 	float z;
+	z=sqrt((x*x)+y*y;
+	return z;
+ }
 
-       return 0;
-  }
-
-    or 
-
-   
+int phase(int x, int y)
+{
+	return sin(x+y);
+	
+}
   */
 int main() {
+
+	//int(*int_fn[4](int,int))={_Add_,_Sub_,Multi_,Power_Division};
+	//int(*int_fn2[2](int,int))={magnitude,phase};	
 	Complex a = {3, 4};
 	Complex b = {5, 12};
 
